3e271308128953abef5507be7377f1398e1ab003
Btrumps <38164652+Btrumps@users.noreply.github.com>
2018-08-30 16:32:09 -0400

    added Entity, so we can add in bullets and other things that have x,y, and id's

--- app.js
+++ app.js
@@ -11,56 +11,67 @@ serv.listen(2000);
 console.log("Server Started.");
 
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
 
-function Player(id) {
+function Entity() {
 	this.x = 250;
 	this.y = 250;
+	this.velX = 0;
+	this.velY = 0;
+	this.id = "";
+
+	this.move = function() {
+		this.updatePosition();
+	}
+
+	this.updatePosition = function() {
+		this.x += this.velX;
+		this.y += this.velY;
+	}
+}
+
+function Player(id) {
+	Entity.call(this);
 	this.id = id;
 	this.number = "" + Math.floor(10 * Math.random());
-
 	this.speed = 10;
-
 	
 	this.pressingRight = false;
 	this.pressingLeft = false;
 	this.pressingUp = false;
 	this.pressingDown = false;
 
+	this.entityMove = this.move;
 	this.move = function() {
-		if (this.pressingUp) {
-			this.y -= this.speed;
-		}
-		if (this.pressingDown) {
-			this.y += this.speed;
-		}
-		if (this.pressingLeft) {
-			this.x -= this.speed;
-		}
-		if (this.pressingRight) {
-			this.x += this.speed;
-		}		
+		this.updateVelocity();
+		this.entityMove();
 	}
+
+	this.updateVelocity = function() {
+		if (this.pressingUp) {
+			this.velY = -this.speed;
+		} else if (this.pressingDown) {
+			this.velY = this.speed;
+		} else {
+			this.velY = 0;
+		}
+
+		if (this.pressingLeft) {
+			this.velX = -this.speed;
+		} else if (this.pressingRight) {
+			this.velX = this.speed;
+		} else {
+			this.velX = 0;
+		}
+	}
+	// Adds this client to the player list whenever the client is created
+	Player.list[id] = this;
 }
+Player.prototype = Object.create(Entity.prototype);
 
-var clientNumber = 0;
+Player.list = {};
 
-
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket) {
-	clientNumber++; // Increases number to make sure no two clients share same id
-	socket.id = clientNumber;
-	SOCKET_LIST[socket.id] = socket;
-
-	// Creates a new player and adds it to our player list
+Player.onConnect = function(socket) {
 	var newPlayer = new Player(socket.id);
-	PLAYER_LIST[socket.id] = newPlayer;
-
-	// This will remove the player from the socket/player list
-	socket.on('disconnect', function() {
-		delete SOCKET_LIST[socket.id];
-		delete PLAYER_LIST[socket.id];
-	});
 
 	socket.on('keyPress', function(data) {
 		if (data.inputId === 'up') {
@@ -73,12 +84,16 @@ io.sockets.on('connection', function(socket) {
 			newPlayer.pressingRight = data.state;
 		}
 	});
-});
+}
 
-setInterval(function() {
+Player.onDisconnect = function(socket) {
+	delete Player.list[socket.id];
+}
+
+Player.update = function() {
 	var pack = []; // Array which will contain all client's info
-	for (var i in PLAYER_LIST) {
-		var player = PLAYER_LIST[i];
+	for (var i in Player.list) {
+		var player = Player.list[i];
 		player.move();
 		pack.push({
 			x: player.x,
@@ -86,6 +101,30 @@ setInterval(function() {
 			number: player.number
 		});
 	}
+
+	return pack;
+}
+
+var clientNumber = 0;
+var io = require('socket.io')(serv,{});
+
+// When a player connects, this function is called
+io.sockets.on('connection', function(socket) {
+	clientNumber++; // Increases number to make sure no two clients share same id
+	socket.id = clientNumber;
+	SOCKET_LIST[socket.id] = socket;	
+
+	Player.onConnect(socket);
+
+	// This will remove the player from the socket/player list
+	socket.on('disconnect', function() {
+		delete SOCKET_LIST[socket.id];
+		Player.onDisconnect(socket);
+	});
+});
+
+setInterval(function() {
+	var pack = Player.update(); // Returns all client information to be passed to each client
 
 	for (var i in SOCKET_LIST) {
 		var socket = SOCKET_LIST[i];
